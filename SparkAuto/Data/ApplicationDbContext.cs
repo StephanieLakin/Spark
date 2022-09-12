206fbb0e0c31d0ef68e12e6cd1866a93ec9d1d3a
unknown <stephanielakin13@outlook.com>
2022-09-12 10:55:19 -0400

    initial commit

---
+++ SparkAuto/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SparkAuto.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SparkAuto.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ServiceType> ServiceType {get; set;}
+
+
+
+    }
+}
